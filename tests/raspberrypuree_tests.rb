219a104e7de2da1db5845f871b69208683292019
kaivong <234134+kaivongcom@users.noreply.github.com>
2026-09-23 19:09:11 +0100

    edit tests

--- tests/raspberrypuree_tests.rb
+++ tests/raspberrypuree_tests.rb
@@ -5,27 +5,27 @@ class TestRaspberrypuree < Minitest::Test
 	def setup
 		params1 = { site_url: 'https://github.com', 
                form_completed: true,
-               username: 'kaichanvong',
+               username: 'kaivongcom',
                nickname: 'kai' }
     params2 = { site_url: 'https://github.com',
-                text: 'here is a example from the user @kaichanvong about his user info of copied text' }
+                text: 'here is a example from the user @kaivongcom about his user info of copied text' }
     @puree1 = Raspberrypuree.new(params1)
 		@puree2 = Raspberrypuree.new(params2)
 	end
 
 	def test_markdown
-    assert_equal('[@kaichanvong](https://github.com/kaichanvong)', @puree1.markdown)
-		assert_equal('[@kaichanvong](https://github.com/kaichanvong)', @puree2.markdown)
+    assert_equal('[@kaivongcom](https://github.com/kaivongcom)', @puree1.markdown)
+		assert_equal('[@kaivongcom](https://github.com/kaivongcom)', @puree2.markdown)
 	end
 
 	def test_uri
-    assert_equal('https://github.com/kaichanvong', @puree1.uri)
-		assert_equal('https://github.com/kaichanvong', @puree2.uri)
+    assert_equal('https://github.com/kaivongcom', @puree1.uri)
+		assert_equal('https://github.com/kaivongcom', @puree2.uri)
 	end
 
 	def test_username
-    assert_equal('kaichanvong', @puree1.username)
-		assert_equal('kaichanvong', @puree2.username)
+    assert_equal('kaivongcom', @puree1.username)
+		assert_equal('kaivongcom', @puree2.username)
 	end
 
 	def test_firstname
